0da2242685b5af1f55b12ac29d6bf11fa785f7f6
Adrian <adriansb3105@gmail.com>
2017-06-16 09:25:53 -0600

    Credentials corrected

--- core/ConexionDB.php
+++ core/ConexionDB.php
@@ -3,16 +3,20 @@ class ConexionDB{
   private $conn;
 
   public function __construct(){
-    $this -> conn = mysqli_connect('us-cdbr-iron-east-03.cleardb.net', 'b9374b69eb27a1', 'd44752f5');
-    if(!$this -> conn){
-      echo 'Problemas de conexion al servidor';
-      exit();
-    }
-    mysqli_select_db($this -> conn, 'heroku_1dc4dabb03f9b5c');
+    $this -> conn = mysqli_connect('us-cdbr-iron-east-03.cleardb.net', 'bf98e650d2c1e8', '4484b84b', 'heroku_6c4e448085e0350', '3306');
+    //if(!$this -> conn){
+    //  echo 'Problemas de conexion al servidor';
+    //  exit();
+    //}
+
+  if (mysqli_connect_errno()){
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+    //mysqli_select_db($this -> conn, 'heroku_1dc4dabb03f9b5c');
   }
 
   public function conectar(){
     return $this -> conn;
   }
 }
-
