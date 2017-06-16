e0139b9ab4bbd37e275520ddec83ee857770afa9
Adrian <adriansb3105@gmail.com>
2017-06-16 09:09:18 -0600

    Test

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<!--
 
-include_once 'controller/DefaultController.php';
 
-$controller = new DefaultController();
-$controller->invoke();
+
+	//include_once 'controller/DefaultController.php';
+
+	//$controller = new DefaultController();
+	//$controller->invoke();
+-->
+<h1>hola</h1>
+
+
+</body>
+</html>
